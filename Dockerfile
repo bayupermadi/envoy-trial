06e254e2dcf96887bd896a16e0d2dffd9808e5dc
bayupermadi <irwanhikmah@gmail.com>
2019-12-20 16:45:10 +0700

    initial push

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM envoyproxy/envoy-dev:b8558af80c7cfbe59c235b9137c4c664448f73e0
+COPY envoy.yaml /etc/envoy/envoy.yaml
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# docker pull envoyproxy/envoy-dev:b8558af80c7cfbe59c235b9137c4c664448f73e0
+image_id=`docker images | grep -v "IMAGE ID" | grep v1 | awk '{print $3}'`
+container_id=`docker ps | grep -v "CONTAINER" | grep v1 | awk '{print $1}'`
+docker stop $container_id
+docker rmi $image_id
+docker build -t envoy:v1 .
+docker run -d -p 9901:9901 -p 10000:10000 envoy:v1
+sleep 5
+curl http://localhost:10000
